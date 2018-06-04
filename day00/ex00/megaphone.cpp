a151a9e2308a3b031e0f2aec4dae098f5c4554ee
Afrika MATSHIYE <amatshiy@e4r22p3.wethinkcode.co.za>
2018-06-04 13:43:23 +0200

    42 header added

--- day00/ex00/megaphone.cpp
+++ day00/ex00/megaphone.cpp
@@ -1,10 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amatshiy <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/04 08:07:24 by amatshiy          #+#    #+#             */
+/*   Updated: 2018/06/04 08:07:56 by amatshiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 
 int main(int ac, char **av)
 {
+	char c;
+
 	if (ac == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
 	}
 	else
 	{
@@ -12,9 +27,9 @@ int main(int ac, char **av)
 		{
 			for (int j = 0; av[i][j] != '\0'; j++)
 			{
-				if (av[i][j] >= 'a' && 'z' >= av[i][j])
+				if (av[i][j] >= 'a' && av[i][j] <= 'z')
 				{
-					char c = av[i][j] - 32;
+					c = av[i][j] - 32;
 					std::cout << c;
 				}
 				else
@@ -23,8 +38,7 @@ int main(int ac, char **av)
 				}
 			}
 		}
-		std::cout << "\n";
+		std::cout << std::endl;
 	}
-
 	return (0);
 }
